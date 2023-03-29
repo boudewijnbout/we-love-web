020ed19144c6ee095aebe14697e53fc7dd7ba1f5
boudewijnbout <boudewijn_bout@outlook.com>
2023-03-29 09:52:25 +0200

    Added wait property to general app

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -5,7 +5,7 @@ import {AnimatePresence} from "framer-motion";
 
 export default function App({Component, pageProps, router}) {
     return (
-        <AnimatePresence>
+        <AnimatePresence wait={true}>
             <Component {...pageProps} key={router.asPath}/>
         </AnimatePresence>
     )
